68a5ec90e3c08a7c083645df7c646a552fb875eb
Fábio Oliveira <chrystianfabio.oliveira@gmail.com>
2026-05-19 19:53:41 -0300

    feat: implementa listagem dinâmica de personagens da API

--- index.php
+++ index.php
@@ -33,9 +33,47 @@ fetch("https://rickandmortyapi.com/api/character")
 
 .then(data => {
 
-    console.log(data);
+    const container = document.getElementById("characters");
 
-});
+    data.results.forEach(character => {
+
+        container.innerHTML += `
+        
+            <div class="col-md-3 mb-4">
+
+                <div class="card bg-secondary text-white h-100">
+
+                    <img src="${character.image}" 
+                         class="card-img-top">
+
+                    <div class="card-body d-flex flex-column text-center">
+
+                        <h5 class="card-title">
+                            ${character.name}
+                        </h5>
+
+                        <p>
+                            ${character.species}
+                        </p>
+
+                        <a href="detalhes.php?api_url=${character.url}"
+                           class="btn btn-primary mt-auto">
+
+                            Ver detalhes
+
+                        </a>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        `;
+
+    });
+
+})
 
 </script>
 
